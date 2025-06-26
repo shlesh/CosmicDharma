a29a271b9204aa346c0b936b322c52b6dc66106d
Shailesh Tiwari <23shlesh@gmail.com>
2025-06-27 05:00:11 +0530

    Replace star textures with SVG

---
+++ components/StarryBackground.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function StarryBackground() {
+  return (
+    <>
+      <div className="star-layer layer1" aria-hidden="true"></div>
+      <div className="star-layer layer2" aria-hidden="true"></div>
+      <div className="star-layer layer3" aria-hidden="true"></div>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Navbar from '../components/Navbar';
 import ToastProvider from '../components/ToastProvider';
+import StarryBackground from '../components/StarryBackground';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ToastProvider>
+      <StarryBackground />
       <Navbar />
       <Component {...pageProps} />
     </ToastProvider>
